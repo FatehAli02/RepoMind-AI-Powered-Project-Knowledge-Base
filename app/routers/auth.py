78fa43248e7a4f3fe814e481f2dba977c153c5cc
Fateh Ali <fatehaliyasin@gmail.com>
2026-07-23 17:11:07 +0500

    Auth Endpoints

---
+++ app/routers/auth.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserResponse
+from app.core.security import hash_password, verify_password, create_access_token
+
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def register(user_in: UserCreate, db: Session = Depends(get_db)):
+
+    if db.query(User).filter(User.email == user_in.email).first():
+        raise HTTPException(status_code=400, detail="Email Already Registered")
+
+    hashed_pwd = hash_password(user_in.password)
+    new_user = User(email=user_in.email, hash_password=hashed_pwd)
+
+    try:
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+    return new_user
+
+@router.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == form_data.username).first()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"})
+
+    access_token = create_access_token(data={"sub" : user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+    
